581924e63884476afd969d46bd149ee59c54adb1
Christos KK Loverdos <loverdos@gmail.com>
2020-11-21 16:01:18 +0200

    Update fish; add build/run scripts

--- base-install-fisherman.sh
+++ base-install-fisherman.sh
@@ -2,6 +2,18 @@
 
 set -eux
 
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fish -c 'fisher add fisherman/stark'
-fish -c 'fisher add fishpkg/fish-prompt-metro'
+fish -c 'curl -sL git.io/fisher | source && fisher install jorgebucaran/fisher'
+fish -c 'fisher install fishpkg/fish-segment'
+fish -c 'fisher install fishpkg/fish-pwd-info'
+fish -c 'fisher install fishpkg/fish-host-info'
+fish -c 'fisher install fishpkg/fish-git-util'
+fish -c 'fisher install fishpkg/fish-last-job-id'
+fish -c 'fisher install fishpkg/fish-humanize-duration'
+fish -c 'fisher install fishpkg/fish-pwd-is-home'
+fish -c 'fisher install fisherman/stark'
+fish -c 'fisher install fishpkg/fish-prompt-metro'
+fish -c 'fisher install laughedelic/pisces'
+fish -c 'fisher install edc/bass'
+
+ls -al ~/.config/fish
+
